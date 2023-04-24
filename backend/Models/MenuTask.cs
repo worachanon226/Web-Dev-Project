bb18e8e22df637bdbfde9a9e289f89e591f8d363
PuNe <worachanon226@hotmail.com>
2023-04-24 20:52:01 +0700

    Update menuTask

--- backend/Models/MenuTask.cs
+++ backend/Models/MenuTask.cs
@@ -5,7 +5,8 @@ public class MenuTask
     public string Id { get; set; } = string.Empty;
     public string UserId { get; set; } = string.Empty;
     public string Store { get; set; } = string.Empty;
-    public Models.Menu Menu { get; set; } = new Models.Menu();
+    public string Name { get; set; } = string.Empty;
+    public int Price { get; set; } = 0;
     public int Amount { get; set; } = 0;
     public bool IsConfirm { get; set; } = false;
     public string Comment { get; set; } = string.Empty;
