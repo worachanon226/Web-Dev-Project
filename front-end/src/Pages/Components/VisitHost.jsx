5d022b0b84481c7e40bf5df10977ccb67a3aa7ad
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2023-04-27 22:14:05 +0700

    ีupdate

--- front-end/src/Pages/Components/VisitHost.jsx
+++ front-end/src/Pages/Components/VisitHost.jsx
@@ -4,6 +4,7 @@ import { IoCloseSharp } from 'react-icons/io5';
 import { GrFormAdd } from 'react-icons/gr'
 import { useParams } from "react-router-dom";
 import React, { useState } from "react";
+import './style/VisitHost.css'
 
 const VisitHost = () => {
   let [menu, setMenu] = useState();
@@ -13,51 +14,39 @@ const VisitHost = () => {
     return <div>Loading</div>;
   }
   return (
-    <div className="bg-primary d-flex flex-column align-items-center w-100 h-100 flex-wrap">
-      <AddMenuModal callback={setMenu} taskId={hostId} />
-      <div className="m-5 rounded" style={{ width: '60%', height: '245px', background:'#FF8066' }}>
+    <div className=" d-flex flex-column align-items-center w-100 h-100 flex-wrap">
+      <AddMenuModal class='textadd'callback={setMenu} taskId={hostId} />
+      <div className="m-5 rounded cardout">
+      <div className="d-flex align-self w-auto rounded justify-content-between m-3 text-center flex-wrap textCardOut">
+              <h4 className="bg-light rounded">ชื่ออาหาร</h4>
+              <h4 className="bg-light rounded">จำนวน</h4>
+              <h4 className="bg-light rounded">หมายเหตุ</h4>
+              <IoCloseSharp class='iconVisit' />
+            </div>
         {menu.map((e) => (
           <div className="align-self rounded flex-wrap" key={e.id}>
-            <div className="d-flex align-self w-auto rounded justify-content-between m-3 text-center flex-wrap">
-              <h4 className="bg-light rounded" style={{ fontSize: '18px',width:'30%',height:'30px'}}>ชื่ออาหาร</h4>
-              <h4 className="bg-light rounded" style={{ fontSize: '18px',width:'30%'}}>จำนวน</h4>
-              <h4 className="bg-light rounded" style={{ fontSize: '18px',width:'30%'}}>หมายเหตุ</h4>
-              <IoCloseSharp size={30} style={{ background: 'red', color: 'white' }} />
-            </div>
 
-            <div className="d-flex align-self w-auto rounded justify-content-between m-3 text-center flex-wrap">
-              <input className="rounded" style={{ fontSize: '18px',width:'30%',alignContent:'center'}} placeholder="Name" />
-              <input className="rounded" style={{ fontSize: '18px',width:'30%' }} placeholder="Amount" />
-              <input className="rounded" style={{ fontSize: '18px',width:'30%' }} placeholder="Comment" />
+            <div className="d-flex align-self w-auto rounded justify-content-between m-3 text-center flex-wrap textCardIn">
+              <input className="rounded " placeholder="Name" />
+              <input className="rounded"  placeholder="Amount" />
+              <input className="rounded" placeholder="Comment" />
               <IoCloseSharp size={30} style={{ background: 'red', color: 'white' }} />
-            </div>
-
-            <div className="d-flex align-self w-auto rounded justify-content-between m-3 text-center flex-wrap">
-              <input className="rounded" style={{ fontSize: '18px',width:'30%',alignContent:'center'}} placeholder="Name" />
-              <input className="rounded" style={{ fontSize: '18px',width:'30%' }} placeholder="Amount" />
-              <input className="rounded" style={{ fontSize: '18px',width:'30%' }} placeholder="Comment" />
-              <IoCloseSharp size={30} style={{ background: 'red', color: 'white' }} />
-            </div>
-
-            
-            <div className="d-flex justify-content-center">
-              <GrFormAdd size={25} style={{ background: 'white', borderRadius:'10px'}}/>
             </div>
 
             <div className="d-flex gap-3 justify-content-center">
-            <button className="rounded bg-light m-3" style={{fontSize:'18px',width:'10%',alignContent:'center'}} type="submit">ยืนยัน</button>
-          </div>
+              <button className="rounded bg-light m-3 submitVisit" type="submit">ยืนยัน</button>
+            </div>
 
-            {/* <span  style={{background:'purple'}}>{JSON.stringify(e)}</span>
+            <span style={{ background: 'purple' }}>{JSON.stringify(e)}</span>
             <button
-              style={{ marginLeft: "auto", background: "green" }}
+              className="d-flex gap-3 justify-content-center"
               onClick={async () => {
                 await deleteMenu(hostId, e.id);
                 getTask(hostId, setMenu);
               }}
             >
               Delete
-            </button> */}
+            </button>
           </div>
         ))}
       </div>
