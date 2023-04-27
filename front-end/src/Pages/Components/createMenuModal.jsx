8ec52931b5bbfaf20d8fe96a40e6db1ec9b51d3d
10Dev <today13113@gmail.com>
2023-04-27 17:04:27 +0700

    update

--- front-end/src/Pages/Components/createMenuModal.jsx
+++ front-end/src/Pages/Components/createMenuModal.jsx
@@ -3,6 +3,7 @@ import { IoCloseSharp } from 'react-icons/io5';
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { useUserContext } from "../../userContext";
+import Modal from "react-modal";
 function AddMenuModal({ callback, taskId }) {
   let {user} = useUserContext()
   const customStyles = {
