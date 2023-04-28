1eeeb3ff75d37814817418162c7de21cef1a7b75
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2023-04-29 00:07:46 +0700

    update

--- front-end/src/Pages/Components/Item.jsx
+++ front-end/src/Pages/Components/Item.jsx
@@ -5,8 +5,8 @@ import './style/Item.css'
 let Item = ({data,control}) => {
   return (
     <div className='boxStyle'>
-      <p style={{marginTop:'10px'}}>โรงอาหาร:{data.canteen}</p>
-      <p style={{marginTop:'10px'}}>รวม:{data.totalPrice}</p>
+      <p>โรงอาหาร:{data.canteen}</p>
+      <p>รวม:{data.totalPrice}</p>
       <Link className="btJoin" to={data.id}>JOIN</Link>
       {control}
       
