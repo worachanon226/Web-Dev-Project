85d75414121099f043b5609034c8116018eff7eb
10Dev <today13113@gmail.com>
2023-04-28 07:56:46 +0700

    update

--- front-end/src/Pages/Components/Nav.jsx
+++ front-end/src/Pages/Components/Nav.jsx
@@ -1,121 +1,143 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Outlet, Link } from "react-router-dom";
-import { AiFillHome } from 'react-icons/ai';
-import { IoFastFood } from 'react-icons/io5';
-import { SiGnuprivacyguard } from 'react-icons/si';
-import { useEffect } from 'react';
-import './style/Nav.css'
-import $ from 'jquery';
-import { useUserContext } from '../../userContext';
+import { AiFillHome } from "react-icons/ai";
+import { IoFastFood } from "react-icons/io5";
+import { SiGnuprivacyguard } from "react-icons/si";
+import { useEffect } from "react";
+import "./style/Nav.css";
+import $ from "jquery";
+import { useUserContext } from "../../userContext";
 
 const Nav = () => {
-  let {user,setUser } = useUserContext()
+  let { user, setUser } = useUserContext();
   function animation() {
-
-    var tabsNewAnim = $('#navbarSupportedContent')
-    var activeItemNewAnim = tabsNewAnim.find('.active');
+    var tabsNewAnim = $("#navbarSupportedContent");
+    var activeItemNewAnim = tabsNewAnim.find(".active");
     var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
     var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
     var itemPosNewAnimTop = activeItemNewAnim.position();
     var itemPosNewAnimLeft = activeItemNewAnim.position();
     $(".hori-selector").css({
-      "top": itemPosNewAnimTop.top + "px",
-      "left": itemPosNewAnimLeft.left + "px",
-      "height": activeWidthNewAnimHeight + "px",
-      "width": activeWidthNewAnimWidth + "px"
+      top: itemPosNewAnimTop.top + "px",
+      left: itemPosNewAnimLeft.left + "px",
+      height: activeWidthNewAnimHeight + "px",
+      width: activeWidthNewAnimWidth + "px",
     });
-    $("#navbarSupportedContent").on("click", "li", function (e) {
-      $('#navbarSupportedContent ul li').removeClass("active");
-      $(this).addClass('active');
+    $("#navbarSupportedContent").on("click", "ul li", function (e) {
+      $("#navbarSupportedContent ul li").removeClass("active");
+      $(this).addClass("active");
       var activeWidthNewAnimHeight = $(this).innerHeight();
       var activeWidthNewAnimWidth = $(this).innerWidth();
       var itemPosNewAnimTop = $(this).position();
       var itemPosNewAnimLeft = $(this).position();
       $(".hori-selector").css({
-        "top": itemPosNewAnimTop.top + "px",
-        "left": itemPosNewAnimLeft.left + "px",
-        "height": activeWidthNewAnimHeight + "px",
-        "width": activeWidthNewAnimWidth + "px"
+        top: itemPosNewAnimTop.top + "px",
+        left: itemPosNewAnimLeft.left + "px",
+        height: activeWidthNewAnimHeight + "px",
+        width: activeWidthNewAnimWidth + "px",
       });
     });
   }
 
   useEffect(() => {
-
     animation();
-    $(window).on('resize', function () {
-      setTimeout(function () { animation(); }, 700);
+    $(window).on("load", function () {
+      setTimeout(function () {
+        animation();
+      }, 100);
     });
-
+    $(window).on("resize", function () {
+      setTimeout(function () {
+        animation();
+      }, 100);
+    });
   }, []);
-
+  let l = {
+    width: "135px",
+  };
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-mainbg">
-
         <Link className="text" to="/" exact>
           Hew-Hew
         </Link>
 
-        <button className="navbar-toggler collapsed border-0"
+        <button
+          className="navbar-toggler collapsed border-0"
           onClick={function () {
-            setTimeout(function () { animation(); });
+            setTimeout(function () {
+              animation();
+            }, 1);
           }}
-          type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+        >
           <span class="navbar-toggler-icon"></span>
         </button>
 
         <div
-          className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarSupportedContent"
+        >
           <ul className="navbar-nav ml-auto">
-
             <div className="hori-selector">
               <div className="left"></div>
               <div className="right"></div>
             </div>
 
-            <li className="nav-item active">
-              <Link className="nav-link d-flex" to="/" >
+            <li  className="nav-item  active">
+              <Link className="nav-link d-flex" to="/">
                 <i className="fas fa-tachometer-al m-1">
                   <AiFillHome />
                 </i>
-                <h5 className='text-center'>Home</h5>
+                <h5 className="text-center">Home</h5>
               </Link>
             </li>
 
-            <li className="nav-item">
-              <Link className="nav-link d-flex" to="/service" >
+            <li  className="nav-item">
+              <Link className="nav-link d-flex" to="/service">
                 <i className="far fa-tachometer-alt m-1">
                   <IoFastFood />
                 </i>
-                <h5 className='text-center'>Services</h5>
+                <h5 className="text-center">Services</h5>
               </Link>
             </li>
 
-            <li className="nav-item">
-              <Link className="nav-link d-flex" to="/profile" >
+            <li  className="nav-item">
+              <Link className="nav-link d-flex" to="/profile">
                 <i className="far fa-clone m-1">
                   <IoFastFood />
                 </i>
-                <h5 className='text-center'>Profile</h5>
+                <h5 className="text-center">Profile</h5>
               </Link>
             </li>
 
-            <li className="nav-item">
-              {!user ? <Link className="nav-link d-flex" to="/login" >
-                <i className="fas far fa-chart-bar m-1">
-                  <SiGnuprivacyguard />
-                </i>
-                <h5 className='text-center'>Sign Up</h5>
-              </Link> :<button className='text-center nav-link d-flex' onClick={()=>{
-                setUser(undefined)
-              }} ><h3>Log out</h3></button>}
+            <li  className="nav-item">
+              {!user ? (
+                <Link className="nav-link d-flex" to="/login">
+                  <i className="fas far fa-chart-bar m-1">
+                    <SiGnuprivacyguard />
+                  </i>
+                  <h5 className="text-center">Sign In</h5>
+                </Link>
+                
+              ) : (
+                <Link onClick={()=>{
+                  setUser(undefined)
+                }} className="nav-link d-flex" to="/login">
+                  <i className="fas far fa-chart-bar m-1">
+                    <SiGnuprivacyguard />
+                  </i>
+                  <h5 className="text-center">Log Out</h5>
+                </Link>
+              )}
             </li>
           </ul>
         </div>
       </nav>
       <Outlet />
     </>
-  )
-}
-export default Nav
+  );
+};
+export default Nav;
--- front-end/src/Pages/Home.jsx
+++ front-end/src/Pages/Home.jsx
@@ -1,35 +1,34 @@
 import { Link } from "react-router-dom";
 import React from "react";
-import './style/Home.css'
-
+import "./style/Home.css";
 
 function Home() {
-  
   return (
-    <div className="d-flex flex-column align-center-center justify-content-center w-100 h-100">
-      <div className="d-flex flex-row justiify-content-between h-75 w-100">
-       
-    <div className="container">
-    <div className="card">
-      <Link to="Host"><div className="box1">
-        <div className="content">
-          <h4>HOST</h4>
-        </div>
-      </div></Link>
-    </div>
+    <div className="d-flex flex-column align-center-center justify-content-center w-100 h-">
+      <div className="d-flex flex-row justiify-content-between h-75 w-100 ">
+        <div className="container">
+          <div className="card">
+            <Link to="Host">
+              <div className="box1">
+                <div className="content">
+                  <h4>HOST</h4>
+                </div>
+              </div>
+            </Link>
+          </div>
 
-    <div className="card">
-    <Link to="Guest"><div className="box2">
-        <div className="content">
-        <h4>JOIN</h4>
+          <div className="card">
+            <Link to="Guest">
+              <div className="box2">
+                <div className="content">
+                  <h4>JOIN</h4>
+                </div>
+              </div>
+            </Link>
+          </div>
         </div>
-      </div></Link>
+      </div>
     </div>
-   
-      </div>
-      </div>
-      </div>
-
   );
 }
 
