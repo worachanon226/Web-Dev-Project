f3839db78ebc0d526b947e94ab07e62540b7be4d
patiphan01 <86480648+paan1@users.noreply.github.com>
2023-04-29 02:56:38 +0700

    update

--- front-end/src/Pages/Components/ManageHost.jsx
+++ front-end/src/Pages/Components/ManageHost.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { getTask } from "../../Controller/HostController";
 import List from "./List";
+import './style/ManageHost.css'
+
+
 const ManageHost = () => {
   let [task, setTask] = useState();
   let { hostId } = useParams();
@@ -10,11 +13,21 @@ const ManageHost = () => {
     return <div>Loading</div>;
   }
   return (
-    <div >
-      {task.menus.map((e) => (
-        <List key={e.id} props={e} callback = {setTask} hostId = {hostId}></List>
-      ))}
+    <div className="containerM">
+
+      <h2>Menu</h2> 
+
+      <div className="Card"></div>
+      <div className="Card"></div>
+      <div className="Card"></div>
+
+      {task.menus &&
+        task.menus.map((e) => (
+          <List key={e.id} props={e} callback={setTask} hostId={hostId}></List>
+        ))}
+      {task.menus === [] && <h3>Waiting for request Menu</h3>}
     </div>
+
   );
 };
 
