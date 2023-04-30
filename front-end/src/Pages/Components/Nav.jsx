168cf9555e658c6fa22aecbb15d345e74cf0e6c2
patiphan01 <86480648+paan1@users.noreply.github.com>
2023-04-30 18:45:41 +0700

    update

--- front-end/src/Pages/Components/Nav.jsx
+++ front-end/src/Pages/Components/Nav.jsx
@@ -8,6 +8,7 @@ import { useEffect } from "react";
 import "./style/Nav.css";
 import $ from "jquery";
 
+
 const Nav = () => {
   let { user, setUser } = useUserContext();
   function animation() {
@@ -146,7 +147,7 @@ const Nav = () => {
 
         <Link className="nav-link d-flex" to='/'>
           <i className="fas far fa-chart-bar m-1">
-            <img className="picturboxlayerNav" src=''  />
+          <img className="logoNav" src="https://cdn.discordapp.com/attachments/1067613078163820554/1102185039233560667/395ec107171e462e62310e6c9b1b0fa2.png" />
           </i>
           <h3 className="text-center">Hew-Hew</h3>
         </Link>
--- front-end/src/Pages/Components/VisitHost.jsx
+++ front-end/src/Pages/Components/VisitHost.jsx
@@ -2,7 +2,7 @@ import { deleteMenu, getTask } from "../../Controller/GuestController";
 import { useUserContext } from "../../userContext";
 import { AddMenuModal } from "./createMenuModal";
 import { useParams } from "react-router-dom";
-import {GrFormAdd} from 'react-icons/gr';
+import { GrFormAdd } from 'react-icons/gr';
 import React, { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import "./style/VisitHost.css";
@@ -15,21 +15,40 @@ const VisitHost = () => {
     getTask(hostId, setMenu);
     return <div>Loading</div>;
   }
+  
   return (
     <div className="d-flex flex-column align-items-center flex-wrap bgVisitHost">
       <AddMenuModal callback={setMenu} taskId={hostId} />
       <div className="mt-5 rounded cardoutVisit ">
-        {menu.map((e) => (
-          <List
-          key={e.id}
-          props={e}
-          userId = {user.id}
-          callback={setMenu}
-          hostId={hostId}
-        ></List>
-        // {add delete Button}
-        ))}
         
+        {menu.map((e) => (
+          e.userId === user.id && (
+          <div>
+            <List
+              key={e.id}
+              props={e}
+              userId={user.id}
+              callback={setMenu}
+              hostId={hostId}
+            ></List>
+
+
+            {
+                <button
+                  style={{ marginLeft: "auto", background: "green" }}
+                  onClick={async () => {
+                    await deleteMenu(hostId, e.id);
+                    getTask(hostId, setMenu);
+                  }}
+                >
+                  Delete
+                </button>
+
+             
+            } </div>
+
+        )))}
+
       </div>
     </div>
 
