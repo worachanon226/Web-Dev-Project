6cfdbea8f6dcf8ef7933937bd7cb6b860360ff2a
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2023-05-01 06:36:54 +0700

    Merge branch 'frond-end-upgrade-UI' of https://github.com/worachanon226/Web-Dev-Project into frond-end-upgrade-UI

--- front-end/src/Pages/Components/LoadingTask.jsx
+++ front-end/src/Pages/Components/LoadingTask.jsx
@@ -9,7 +9,9 @@ const LoadingTask = () => {
           src="https://cdn.dribbble.com/users/1478651/screenshots/9683496/media/b921a805a76e70f0c36f0410310be009.gif"
           alt="hew"
         />
+        <div>
         <h2 className='text-center'>Waiting Order...</h2>
+        </div>
         </div>
   )
 }
